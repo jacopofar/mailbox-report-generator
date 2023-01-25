0be74ed4b2f688aae0ac873055dd64c1d4a991c6
Jacopo Farina <jacopo1.farina@gmail.com>
2023-01-25 16:35:43 +0100

    Add back test and coverage after stripping setup.py

--- tests/unit/test_activity_over_time.py
+++ tests/unit/test_activity_over_time.py
@@ -11,17 +11,18 @@ def test_happy_path():
     msg["Date"] = 'Fri, 9 Jun 2006 12:34:56 -0200'
     p.process(msg)
     generated = p.report_snippet()
-    assert '"x": ["2006-06-09"], "y": [1]' in generated
+    assert '"x":["2006-06-09"],"y":[1]' in generated
     msg = Message()
     # no payload this time
     msg["Date"] = 'Sun, 13 Jun 2006 12:34:56 -0200'
     p.process(msg)
     generated = p.report_snippet()
-    assert '"x": ["2006-06-09", "2006-06-13"], "y": [1, 1]' in generated
+    assert '"x":["2006-06-09","2006-06-13"],"y":[1,1]' in generated
 
 
 def test_empty_mbox():
     p = ActivityOverTime()
     generated = p.report_snippet()
-    assert '{"type": "scatter", "x": [], "y": []}' in generated
-
+    assert '"type":"scatter"' in generated
+    assert '"x":[]' in generated
+    assert '"y":[]' in generated
--- tests/unit/test_hour_heatmap.py
+++ tests/unit/test_hour_heatmap.py
@@ -11,21 +11,21 @@ def test_happy_path():
     msg["Date"] = 'Fri, 9 Jun 2006 12:34:56 -0200'
     p.process(msg)
     generated = p.report_snippet()
-    assert '"type": "heatmap"' in generated
-    assert '"Mon", "Tue", "Wed"' in generated
-    hrs = [0] * 24
-    assert generated.count(str(hrs)) == 6
+    assert '"type":"heatmap"' in generated
+    assert '"Mon","Tue","Wed"' in generated
+    hrs = ['0'] * 24
+    assert generated.count(','.join(hrs)) == 6
     # element 12 + 2 is 1, because it is UTC
-    hrs[14] = 1
-    assert str(hrs) in generated
+    hrs[14] = '1'
+    assert str(','.join(hrs)) in generated
 
 
 def test_empty_mbox():
     p = DowHourHeatmap()
     generated = p.report_snippet()
-    assert '"type": "heatmap"' in generated
-    assert '"Mon", "Tue", "Wed"' in generated
+    assert '"type":"heatmap"' in generated
+    assert '"Mon","Tue","Wed"' in generated
     # just ensure it's empty all 7 days of the week
-    hrs = [0] * 24
-    assert generated.count(str(hrs)) == 7
+    hrs = ['0'] * 24
+    assert generated.count(','.join(hrs)) == 7
 
